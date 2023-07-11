e5cd5f7a5bac8e6baf86ca3a445196c34dab7ab1
omvamja <136708203+omvamja@users.noreply.github.com>
2023-07-11 15:59:03 +0530

    Update 8_1.C

--- 8_1.C
+++ 8_1.C
@@ -1,1 +1,32 @@
-#
+#include<stdio.h>
+#include<conio.h>
+
+void main ()
+
+{
+
+int i,n,b=1;
+
+printf("enter array size =  ");
+scanf("%d",&n);
+
+
+int count[n];
+
+
+
+for(i=0;i<n;i++){
+    printf("n[%d]= ",i);
+    scanf("%d",&count[n]);
+    b++;
+
+
+}
+
+printf("length of array is %d ",b-1);
+
+getch();
+
+
+
+}
